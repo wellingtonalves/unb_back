72a45a6229c975a6df797efbbff820c3fe31b1e3
Alex Alexandre da Silva Junior <alex.alexandre@redes.unb.br>
2020-07-02 23:26:36 -0300

    [UPP][19283] - incluindo a primeira parte de configuração da arquitetura inicial

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -2,7 +2,7 @@
 
 /** @var \Illuminate\Database\Eloquent\Factory $factory */
 
-use App\User;
+use App\bpk;
 use Faker\Generator as Faker;
 use Illuminate\Support\Str;
 
@@ -17,7 +17,7 @@ use Illuminate\Support\Str;
 |
 */
 
-$factory->define(User::class, function (Faker $faker) {
+$factory->define(bpk::class, function (Faker $faker) {
     return [
         'name' => $faker->name,
         'email' => $faker->unique()->safeEmail,
