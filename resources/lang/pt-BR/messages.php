47f5d2e16c904bbdf9cfbec3bb1c6e6e3f6e3dc5
Alex Alexandre da Silva Junior <alex.alexandre@redes.unb.br>
2020-07-03 00:58:05 -0300

    [UPP][19283] - adicionando traducões em portugues

---
+++ resources/lang/pt-BR/messages.php
@@ -0,0 +1,16 @@
+<?php
+
+return [
+    'error_form' => 'Erro ao validar o formulário. Verifique os campos e tente novamente.',
+    'login_error' => 'Usuário/Senha inválida',
+    'login_inactive' => 'Usuário desabilitado',
+    'not_authorized' => 'Este usuário não tem permissão para acessar esta plataforma.',
+    'list' => 'Lista',
+    'detail' => 'Detalhe',
+    'success_operation' => 'Operação realizada com sucesso.',
+    'error_operation' => 'Operação realizada com erro.',
+    'message_type' => [
+        'erro' => 'error',
+        'sucesso' => 'success'
+    ]
+];
