bac7c04f29970cc63cb8dde0a53a1bf08bda74cf
Alex Alexandre da Silva Junior <alex.alexandre@redes.unb.br>
2020-09-22 22:36:16 -0300

    [UPD][19819] - atualizando as policies e controller

--- app/Policies/CursoPolicy.php
+++ app/Policies/CursoPolicy.php
@@ -7,8 +7,48 @@ use App\Models\Usuario;
 class CursoPolicy extends AbstractPolicy
 {
 
+    /**
+     * @param Usuario $usuario
+     * @return bool
+     */
     public function index(Usuario $usuario)
     {
         return self::check('CURSO_LISTAR', $usuario);
+    }
+
+    /**
+     * @param Usuario $usuario
+     * @return bool
+     */
+    public function show(Usuario $usuario)
+    {
+        return self::check('CURSO_DETALHAR', $usuario);
+    }
+
+    /**
+     * @param Usuario $usuario
+     * @return bool
+     */
+    public function store(Usuario $usuario)
+    {
+        return self::check('CURSO_INCLUIR', $usuario);
+    }
+
+    /**
+     * @param Usuario $usuario
+     * @return bool
+     */
+    public function update(Usuario $usuario)
+    {
+        return self::check('CURSO_EDITAR', $usuario);
+    }
+
+    /**
+     * @param Usuario $usuario
+     * @return bool
+     */
+    public function destroy(Usuario $usuario)
+    {
+        return self::check('CURSO_EXCLUIR', $usuario);
     }
 }
