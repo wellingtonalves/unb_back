2cfd6b202a899f46b9c97f700db85c39dd4383a7
Guilherme Nunes Gonçalves <guilherme.nunes@redes.unb.br>
2020-11-23 22:16:43 -0300

    #20440 [UPD] - Orientacoes de como integrar com os AVAs

--- app/Services/UsuarioService.php
+++ app/Services/UsuarioService.php
@@ -3,6 +3,7 @@
 namespace App\Services;
 
 use App\Repositories\UsuarioRepository;
+use Symfony\Component\HttpFoundation\Request;
 
 class UsuarioService extends AbstractService
 {
@@ -13,11 +14,38 @@ class UsuarioService extends AbstractService
     protected $repository;
 
     /**
+     * @var MoodleService
+     */
+    protected $moodleService;
+
+    /**
+     * @var CanvasService
+     */
+    protected $canvasService;
+
+    /**
      * CursoService constructor.
      * @param UsuarioRepository $repository
      */
-    public function __construct(UsuarioRepository $repository)
+    public function __construct(UsuarioRepository $repository, MoodleService $moodleService, CanvasService $canvasService)
     {
         $this->repository = $repository;
+        $this->moodleService = $moodleService;
+        $this->canvasService = $canvasService;
+    }
+
+    public function update(Request $request)
+    {
+
+        /**
+         * Consulta inscricoes que o usuario ja realizou
+         * Criar: Model Inscricao e Repository Inscricao que se relaciona com a oferta
+         * 
+         * tb_inscricao > tb_oferta > tb_ava
+         */
+
+        // Para fazer a chamada na API dos AVAs
+        $service = strtolower($consulta->tp_ava).'Service';
+        $metodoInfoSite = 'atualizaUsuario' . ucfirst(strtolower($request->tp_ava));
     }
 }
