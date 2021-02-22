d302acb9d4020cf5996c3c651de2169612908934
Fábio Rocha <fabio.gomes@ifrn.edu.br>
2021-02-22 09:51:25 -0300

    Customizando telas de Validacao de Documentos e Comprovante de Inscricao

--- resources/views/inscricao/modelo01.blade.php
+++ resources/views/inscricao/modelo01.blade.php
@@ -3,83 +3,67 @@
 <head>
     <meta charset="utf-8">
     <title>Comprovante de Inscrição</title>
-    <link href="{{ asset('css/inscricao.css') }}" rel="stylesheet">
+    <link href="{{ base_path('/public/css/inscricao.css') }}" rel="stylesheet">
 </head>
 
 <body>
 
-<h3>COMPROVANTE DE INSCRIÇÃO</h3>
+<header></header>
 
-<div class="info">
-    <h4>CURSO</h4>
-    <p>{{$inscricao['tx_nome_curso']}}</p>
-    <h4>ALUNO</h4>
-    <p>{{$inscricao['tx_nome_pessoa']}}</p>
-    <h4>DADOS DO CURSO</h4>
+<h3>Comprovante de Inscrição</h3>
 
-    <table>
-        <tr>
-            <td>
-                <p>Data da Inscrição</p>
-                <p>{{$inscricao['dt_inscricao']}}</p>
-            </td>
-            <td>
-                <p>Nome da Oferta</p>
-                <p>{{$inscricao['tx_nome_oferta']}}</p>
-            </td>
-        </tr>
-        <tr>
-            <td colspan="2">
-                <p>Instituição Ofertante</p>
-                <p>Instituição Ofertante</p>
-            </td>
-        </tr>
-        <tr>
-            <td colspan="2">
-                <p>Data para Termino do Curso</p>
-                <p>{{$inscricao['dt_termino_oferta']}}</p>
-            </td>
-        </tr>
-        <tr>
-            <td>
-                <p>Carga Horária da Oferta</p>
-                <p>{{$inscricao['qt_carga_horaria_oferta']}}</p>
-            </td>
-            <td>
-                <p>Nota Mínima</p>
-                <p>{{$inscricao['qt_nota_minima_aprovacao']}}</p>
-            </td>
-        </tr>
-        <tr>
-            <td>
-                <p>Modalidade</p>
-                <p>{{$inscricao['tutoria']}}</p>
-            </td>
-            <td>
-                <p>Duração da oferta</p>
-                <p>{{$inscricao['qt_duracao_dias']}}</p>
-            </td>
-        </tr>
-    </table>
+<dl>
+    <dt>Curso</dt>
+    <dd>{{$inscricao['tx_nome_curso']}}</dd>
+    <dt>Aluno</dt>
+    <dd>{{$inscricao['tx_nome_pessoa']}}</dd>
+</dl>
 
-</div>
+<div class="clear"></div>
 
-<div class="rodape">
-    <table>
-        <tr>
-            <td class="qrcode">
-                {{--                {!! DNS2D::getBarcodeHTML(url('/documentos/validacao/certificadocheck/'.$inscricao->nr_codigo_validador), "QRCODE",3,3) !!}--}}
-            </td>
-            <td class="obs">
-                <p>Certificado registrado na Escola Virtual.Gov - EV.G sob o
-                    código {{ $inscricao['nr_codigo_validador'] }}
-                    <br>Este certificado foi gerado em {{ date('d/m/Y')}} às {{ date('H:m')}} horas.
-                    <br>O presente certificado pode ter a sua validade comprovada acessando o QRCode à esquerda, ou,
-                    caso desejar, informando o código acima na opção Validação de Documentos no endereço:
-                    <br>A data de emissão pode ser anterior à data final do curso nos casos em que o participante
-                    alcançou os requisitos mínimos para aprovação antecipadamente.
-                <div class="img-enap"></div>
-            </td>
-        </tr>
-    </table>
-</div>
+<h2>Dados do Curso</h2>
+<table>
+    <tr>
+        <td><strong>Data da Inscrição</strong> {{$inscricao['dt_inscricao']}}</td>
+        <td><strong>Nome da Oferta</strong> {{$inscricao['tx_nome_oferta']}}</td>
+    </tr>
+    <tr>
+        <td colspan="2"><strong>Instituição Ofertante</strong> TODO</td>
+    </tr>
+    <tr>
+        <td colspan="2"><strong>Data para Término do Curso</strong> {{$inscricao['dt_termino_oferta']}}</td>
+    </tr>
+    <tr>
+        <td><strong>Carga Horária da Oferta</strong> {{$inscricao['qt_carga_horaria_oferta']}}</td>
+        <td><strong>Nota Mínima</strong> {{$inscricao['qt_nota_minima_aprovacao']}}</td>
+    </tr>
+    <tr>
+
+     <td><strong>Modalidade</strong> {{$inscricao['tutoria']}}</td>
+     <td><strong>Duração da oferta</strong> {{$inscricao['qt_duracao_dias']}} dias</td>
+
+    </tr>
+</table>
+
+<div class="clear"></div>
+
+<footer>
+
+    <h2>EV.G | Escola Virtual.Gov</h2>
+    <p>Uma proposta de solução para a oferta de capacitação a distância no serviço público brasileiro.</p>
+
+    <div>
+        <table>
+            <tr>
+                <td class="qrcode">
+                    {{--                {!! DNS2D::getBarcodeHTML(url('/documentos/validacao/certificadocheck/'.$inscricao->nr_codigo_validador), "QRCODE",3,3) !!}--}}
+                </td>
+                <td class="organization">O presente documento pode ter a sua validade comprovada acessando o QRCode à esquerda, ou, caso desejar, informando o código <code>{{ $inscricao['nr_codigo_validador'] }}</code> na página da <a href='{{ url('/') }}'>EVG</a>, opção "Validação de Documentos".</td>
+            </tr>
+        </table>
+    </div>
+
+</footer>
+
+</body>
+</html>
